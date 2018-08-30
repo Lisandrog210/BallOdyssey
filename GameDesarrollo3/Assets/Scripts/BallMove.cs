64f87a197893400c20d13a357e27da1478b68713
Lisandro <li_gueva@hotmail.com>
2018-08-30 18:06:12 -0300

    Input Manager, lvl1

--- GameDesarrollo3/Assets/Scripts/BallMove.cs
+++ GameDesarrollo3/Assets/Scripts/BallMove.cs
@@ -21,16 +21,15 @@ public class BallMove : MonoBehaviour{
     void Update()
     {
 
-        
-
-
     }
 
     private void FixedUpdate()
     {
 		//rb.velocity = new Vector2(Input.acceleration.x * moveSpeed, fallingSpeed);
-		rb.AddForce(Vector2.right * Input.acceleration.x * moveSpeed,ForceMode2D.Impulse);
+		//rb.AddForce(Vector2.right * Input.acceleration.x * moveSpeed,ForceMode2D.Impulse);
 		this.transform.rotation = Quaternion.identity;
+		rb.AddForce(Vector2.right* InputManager.Instance.GetHorizontalAxis() * 100.0f * Time.deltaTime,ForceMode2D.Impulse);
 	}
 }
+
 
