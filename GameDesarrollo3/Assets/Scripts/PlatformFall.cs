5ea14de7f9f67f5e3ff41e2db20189d5d0cdd36a
Unknown <li_gueva@hotmail.com>
2018-09-20 12:59:47 -0300

    TPDesarrollo3-v0.1.5
    Agregado PlatformFall y correccion de Prefabs

---
+++ GameDesarrollo3/Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformFall : MonoBehaviour
+{
+
+    public float fallDelay = 1f;
+
+
+    private Rigidbody2D rb2d;
+    private Collider2D col2d;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        col2d = GetComponent<Collider2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Invoke("Fall", fallDelay);
+        }
+    }
+
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+        col2d.enabled = false;
+        
+    }
+
+
+
+}
