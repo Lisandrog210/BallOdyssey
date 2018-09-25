cbfcf91c72649def765a58c58bbb5bf2d7fb1f27
RodrigoPeralta <odrimail@gmail.com>
2018-09-25 13:58:20 -0300

    Spring Script Updated
    Se agrega un script a spring para poder manejar mejor su efecto

---
+++ GameDesarrollo3/Assets/Scripts/Spring.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour {
+
+    private Rigidbody2D colRb;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            colRb = other.gameObject.GetComponent<Rigidbody2D>();
+            colRb.AddForce(Vector2.up * 30, ForceMode2D.Impulse);
+        }
+    }
+
+   
+}
