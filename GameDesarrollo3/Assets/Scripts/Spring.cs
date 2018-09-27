620dab3e712660bdf6efe693eb0ab40e2ec5c7ad
Rodrigo Peralta <odrimail@gmail.com>
2018-09-27 16:39:05 -0300

    Se termina el resorte.

--- GameDesarrollo3/Assets/Scripts/Spring.cs
+++ GameDesarrollo3/Assets/Scripts/Spring.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Spring : MonoBehaviour {
 
-    private Rigidbody2D colRb;
+    private Rigidbody2D colRb;        
+    [SerializeField] GameObject ballGO;
     [SerializeField] float bounceForce = 30f;
 
+
+    
+
     void OnCollisionEnter2D(Collision2D other)
-    {
+    {        
         if (other.gameObject.CompareTag("Ball"))
         {
-            colRb = other.gameObject.GetComponent<Rigidbody2D>();
-            colRb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+            colRb = other.gameObject.GetComponent<Rigidbody2D>();           
+            if (other.contacts[0].normal.y == -1.0f)
+            {
+                colRb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+            }            
         }
     }
 
