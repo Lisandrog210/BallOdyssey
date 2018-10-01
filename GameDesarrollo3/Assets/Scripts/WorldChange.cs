1af6baf82c8a2d2681f3c8e694771a69a1a3aed3
RodrigoPeralta <odrimail@gmail.com>
2018-10-01 20:23:05 -0300

    Se agregan mundos a la pantalla de seleccion de niveles

---
+++ GameDesarrollo3/Assets/Scripts/WorldChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WorldChange : MonoBehaviour
+{
+    public GameObject actualWorld;
+    public GameObject otherWorld1;
+
+
+    private void Awake()
+    {
+        var btn = GetComponent<Button>();
+        btn.onClick.AddListener(ChangeWorld);
+    }
+
+    private void ChangeWorld()
+    {
+        actualWorld.SetActive(true);
+        otherWorld1.SetActive(false);
+    }
+}
+
