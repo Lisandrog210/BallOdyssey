4f0fdd72adf703047bc3e490dbd4404decd82d8c
Unknown <li_gueva@hotmail.com>
2018-11-10 21:09:19 -0300

    btnpause y pausepanel

--- GameDesarrollo3/Assets/Scripts/DestroyPanel.cs
+++ GameDesarrollo3/Assets/Scripts/DestroyPanel.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class DestroyPanel : MonoBehaviour {
 
+    GameObject pauseButton;
+    
     private void Start()
     {
+        pauseButton = GameObject.Find("PauseButton");
+        if (pauseButton)        
+            pauseButton.SetActive(false);
+        
+        
         Time.timeScale = 0;
     }
 
     public void Destroy()
     {
+        pauseButton.SetActive(true);
         this.gameObject.SetActive(false);
         Time.timeScale = 1;
     }
