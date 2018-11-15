e13f7bffbccdc245f98fc5b1f0177961e86789f4
RodrigoPeralta <odrimail@gmail.com>
2018-11-15 18:12:13 -0300

    Se deshace el "arreglo" al movimiento de la pelota y el oncollisionexit de las plataformas por no funcionar como deberia

--- GameDesarrollo3/Assets/Scripts/BallMove.cs
+++ GameDesarrollo3/Assets/Scripts/BallMove.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallMove : MonoBehaviour {
+public class BallMove : MonoBehaviour
+{
 
-    [SerializeField] private float jumpForce = 24.0f;   
-    private Rigidbody2D rb;    
+    [SerializeField] private float jumpForce = 24.0f;
+    private Rigidbody2D rb;
     [SerializeField] private float moveSpeed = 19.0f;
     private bool isGrounded;
     public static BallMove instance;
     private bool moving;
     private float colAngle;
     private float numberOfBounces;
+    Vector2 lastContactPos = new Vector2();
 
     public static BallMove Instance
     {
@@ -27,7 +29,7 @@ public class BallMove : MonoBehaviour {
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();        
+        rb = GetComponent<Rigidbody2D>();
         instance = this;
         numberOfBounces = 1;
     }
@@ -35,18 +37,22 @@ public class BallMove : MonoBehaviour {
     void Update()
     {
         if (InputManager.Instance.GetJumpButton() == true &&
-            isGrounded==true &&
-            !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())       
+            isGrounded == true &&
+            !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+        {           
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        
+            //this.transform.SetParent(null);
+        }
+
+
     }
 
     private void FixedUpdate()
     {
-		//MOVIMIENTO izq-derecha// Si esta en el aire el movimiento es ínfimo----------------------
-		this.transform.rotation = Quaternion.identity;
+        //MOVIMIENTO izq-derecha// Si esta en el aire el movimiento es ínfimo----------------------
+        this.transform.rotation = Quaternion.identity;
 
-        if(isGrounded == true)
+        if (isGrounded == true)
         {
             rb.AddForce(Vector2.right * InputManager.Instance.GetHorizontalAxis() * moveSpeed * Time.deltaTime, ForceMode2D.Impulse);
         }
@@ -56,80 +62,83 @@ public class BallMove : MonoBehaviour {
         }
     }
 
-   
-    void OnCollisionEnter2D(Collision2D collision) 
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.collider.gameObject.layer == LayerMask.NameToLayer("Platforms")) 
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Platforms"))
         {
             isGrounded = true;
             this.transform.SetParent(collision.transform);
             numberOfBounces = 1;
-            lastContactPos = collision.contacts[0].point;
+            //lastContactPos = collision.contacts[0].point;
         }
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("FallingPlatforms"))
         {
             isGrounded = true;
             numberOfBounces = 1;
-            lastContactPos = collision.contacts[0].point;
+            //lastContactPos = collision.contacts[0].point;
         }
         if (collision.collider.gameObject.tag == "FastPlatform")
-        {            
-            rb.AddForce(Vector2.right * moveSpeed*1f, ForceMode2D.Impulse);
+        {
+            rb.AddForce(Vector2.right * moveSpeed * 1f, ForceMode2D.Impulse);
             numberOfBounces = 1;
-            lastContactPos = collision.contacts[0].point;
+            //lastContactPos = collision.contacts[0].point;
         }
         if (collision.collider.gameObject.tag == "FastPlatformx2")
-        {            
+        {
             rb.AddForce(Vector2.right * moveSpeed * 8f, ForceMode2D.Impulse);
             numberOfBounces = 1;
-            lastContactPos = collision.contacts[0].point;
+            //lastContactPos = collision.contacts[0].point;
         }
-        if(collision.collider.gameObject.tag == "MovingPlatform")
+        if (collision.collider.gameObject.tag == "MovingPlatform")
         {
             FrictionJoint2D rb2d = collision.gameObject.GetComponent<FrictionJoint2D>();
             numberOfBounces = 1;
             rb2d.connectedBody = rb;
             collision.gameObject.GetComponent<PlatformMove>().activate = true;
-            lastContactPos = collision.contacts[0].point;
+            //lastContactPos = collision.contacts[0].point;
         }
         if (collision.collider.gameObject.tag == "SmallPlatform")
         {
             FrictionJoint2D rb2d = collision.gameObject.GetComponent<FrictionJoint2D>();
             numberOfBounces = 1;
             rb2d.connectedBody = rb;
-            lastContactPos = collision.contacts[0].point;
+            //lastContactPos = collision.contacts[0].point;
         }
-        
+
         if (collision.gameObject.CompareTag("Spring"))
         {
             colAngle = Vector2.Angle(-collision.contacts[0].normal, new Vector2(collision.transform.up.x, collision.transform.up.y));
-                        
+
             if (colAngle > 120)
             {
-                numberOfBounces += 0.1f;               
-                rb.AddForce((transform.up *30)*numberOfBounces, ForceMode2D.Impulse);                
+                numberOfBounces += 0.1f;
+                rb.AddForce((transform.up * 30) * numberOfBounces, ForceMode2D.Impulse);
             }
 
         }
     }
 
-    void OnCollisionExit2D(Collision2D collision) 
+    void OnCollisionExit2D(Collision2D collision)
     {
 
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Platforms") /*|| 
             collision.collider.gameObject.layer == LayerMask.NameToLayer("FallingPlatforms")*/)
         {
             isGrounded = false;
-            //Debug.Log(isGrounded+" -- " +collision.collider.name);
-            Vector2 aux = new Vector2();
+            //Debug.Log(isGrounded+" -- " +collision.collider.name);            
+            this.transform.SetParent(null);
+            /*Vector2 aux = new Vector2();
             aux.x = this.transform.position.x;
             aux.y = this.transform.position.y;            
-            //Debug.Log("herhe" + Vector2.Distance(aux, lastContactPos));
-            if (Vector2.Distance(aux, lastContactPos)>1.0f)
+            Debug.Log("herhe" + Vector2.Distance(aux, lastContactPos));
+            if (Vector2.Distance(aux, lastContactPos) > 1.25f)
             {
+                Debug.Log("herhe" + Vector2.Distance(aux, lastContactPos));
                 Debug.Log("Bola sale del padre, fue intencional?");
-                this.transform.SetParent(null);
-            }
+                //this.transform.SetParent(null);
+                
+            }*/
 
         }
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("FallingPlatforms"))
@@ -148,7 +157,7 @@ public class BallMove : MonoBehaviour {
             rb2d.connectedBody = null;
         }
     }
-    Vector2 lastContactPos = new Vector2();
+
     void OnCollisionStay2D(Collision2D collision)
     {
 
@@ -157,9 +166,9 @@ public class BallMove : MonoBehaviour {
             isGrounded = true;
             //Debug.Log(isGrounded + " -- " + collision.collider.name);
             this.transform.SetParent(collision.transform);
-            lastContactPos = collision.contacts[0].point;
+            //lastContactPos = collision.contacts[0].point;
         }
-        
+
     }
 }
 
