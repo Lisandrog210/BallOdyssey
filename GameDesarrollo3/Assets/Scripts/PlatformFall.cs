5d9d54f073c974bcf49a8ade8e8a73b960f6ee16
Lisandro <li_gueva@hotmail.com>
2018-11-30 21:03:36 -0300

    Platform Reset

--- GameDesarrollo3/Assets/Scripts/PlatformFall.cs
+++ GameDesarrollo3/Assets/Scripts/PlatformFall.cs
@@ -9,6 +9,7 @@ public class PlatformFall : MonoBehaviour
     private Rigidbody2D rb2d;
     private Collider2D col2d;
     private Vector2 originalPosition;
+    [SerializeField] float _timeToReset;
 
     void Awake()
     {
@@ -29,15 +30,29 @@ public class PlatformFall : MonoBehaviour
     {
         rb2d.bodyType = RigidbodyType2D.Dynamic;
         this.transform.DetachChildren();
-        this.gameObject.SetActive(false);
+        //this.gameObject.SetActive(false);
+        this.gameObject.GetComponent<Collider2D>().enabled = false;
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        StartCoroutine(ExecuteAfterTime(_timeToReset));
     }
 
     public void ResetPosition()
     {
-        CancelInvoke();        
-        this.gameObject.SetActive(true);
+        CancelInvoke();
+        //this.gameObject.SetActive(true);
+        this.gameObject.GetComponent<Collider2D>().enabled = true;
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
         rb2d.velocity = Vector2.zero;
         rb2d.bodyType = RigidbodyType2D.Kinematic;
         this.transform.position = originalPosition;        
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+
+        ResetPosition();
     }
 }
