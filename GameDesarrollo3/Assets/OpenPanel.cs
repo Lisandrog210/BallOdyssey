8f90ed3af85ec734c2947e5899be4c471d3c8944
Lisandro <li_gueva@hotmail.com>
2018-12-04 17:36:02 -0300

    Creditos añadidos al main menu

---
+++ GameDesarrollo3/Assets/OpenPanel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class OpenPanel : MonoBehaviour
+{
+
+    [SerializeField] GameObject panelToOpen;
+
+    void Awake()
+    {
+        var btn = GetComponent<Button>();
+        btn.onClick.AddListener(PanelOpen);
+    }
+
+    private void PanelOpen()
+    {
+        panelToOpen.SetActive(true);
+    }
+}
---
+++ GameDesarrollo3/Assets/ReturnButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ReturnButton : MonoBehaviour
+{
+
+    [SerializeField] GameObject parentPanel;
+
+    void Awake()
+    {
+        var btn = GetComponent<Button>();
+        btn.onClick.AddListener(ReturnAndClose);
+    }
+
+    private void ReturnAndClose()
+    {
+        parentPanel.SetActive(false);        
+    }
+}
