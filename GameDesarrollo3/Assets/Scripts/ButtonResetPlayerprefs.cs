fbfb98a057be1ca2447bfcb8277b28c036fcac10
RodrigoPeralta <odrimail@gmail.com>
2021-02-02 03:28:02 -0300

    Agregar botón de settings en el menú
    Untested

---
+++ GameDesarrollo3/Assets/Scripts/ButtonResetPlayerprefs.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonResetPlayerprefs : MonoBehaviour
+{
+    public GameObject ConfirmationGo;
+    public GameObject MenuGo;
+
+    public void ResetConfirmationText()
+    {
+        MenuGo.SetActive(false);
+        ConfirmationGo.SetActive(true);
+    }
+
+    public void resetYes()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void resetNo()
+    {
+        ConfirmationGo.SetActive(false);
+        MenuGo.SetActive(true);
+    }
+}
+
