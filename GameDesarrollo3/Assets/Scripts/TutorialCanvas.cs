f7df2720d10e13363a9f6b450b0f714a5467344e
RodrigoPeralta <odrimail@gmail.com>
2021-02-26 05:02:02 -0300

    Tutorial funcionando correctamente
    Falta agregar a el primer nivel de cada mundo

---
+++ GameDesarrollo3/Assets/Scripts/TutorialCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCanvas : MonoBehaviour
+{
+    public GameObject tutorialCanvas;
+    private void Start()
+    {       
+        Time.timeScale = 0;
+        if (PlayerPrefs.GetInt("WasTutorialDone") == 1)        
+            this.Destroy();
+        else        
+            tutorialCanvas.SetActive(true);
+    }
+
+    public void Destroy()
+    {        
+        PlayerPrefs.SetInt("WasTutorialDone", 1);
+        Time.timeScale = 1;
+        tutorialCanvas.SetActive(false);
+    }
+}
